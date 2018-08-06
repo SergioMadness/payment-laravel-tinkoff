e804a2dcb0a07e3c3054a9d6615f21a5387e829a
SergioMadness <madness.sergio@gmail.com>
2018-08-06 19:51:34 +0300

    Implement new methods in PayService interface

--- src/drivers/tinkoff/TinkoffDriver.php
+++ src/drivers/tinkoff/TinkoffDriver.php
@@ -334,4 +334,45 @@ class TinkoffDriver implements PayService, TinkoffService
     {
         return $this->getResponseParam('PaymentId');
     }
+
+    /**
+     * Payment system need form
+     * You can not get url for redirect
+     *
+     * @return bool
+     */
+    public function needForm(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Generate payment form
+     *
+     * @param int       $orderId
+     * @param int       $paymentId
+     * @param float     $amount
+     * @param string    $currency
+     * @param string    $paymentType
+     * @param string    $successReturnUrl
+     * @param string    $failReturnUrl
+     * @param string    $description
+     * @param array     $extraParams
+     * @param Arrayable $receipt
+     *
+     * @return string
+     */
+    public function getPaymentForm($orderId,
+                                   $paymentId,
+                                   $amount,
+                                   $currency = self::CURRENCY_RUR,
+                                   $paymentType = self::PAYMENT_TYPE_CARD,
+                                   $successReturnUrl = '',
+                                   $failReturnUrl = '',
+                                   $description = '',
+                                   $extraParams = [],
+                                   $receipt = null)
+    {
+        return '';
+    }
 }
